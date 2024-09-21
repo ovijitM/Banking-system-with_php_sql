fdc971483ed7c5611566fb3790885640cbb3b1cd
MD RAKIB HOSSAIN <141413773+mdrakib719@users.noreply.github.com>
2024-09-22 03:27:39 +0600

    Update loan_de.php

--- User/loan_apply/loan_de.php
+++ User/loan_apply/loan_de.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loan Details</title>
-    <link rel="stylesheet" href="style/loan_de.css">
+    <link rel="stylesheet" href="loan_de.css">
 </head>
 <body>
 
