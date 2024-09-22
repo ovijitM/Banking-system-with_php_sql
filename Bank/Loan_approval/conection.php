b651c1ea44c719d617e859bcc86e4159810a5137
MD RAKIB HOSSAIN <141413773+mdrakib719@users.noreply.github.com>
2024-09-22 18:23:46 +0600

    Update conection.php

--- Bank/Loan_approval/conection.php
+++ Bank/Loan_approval/conection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bankloan";
+$dbname = "echo_bank";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
