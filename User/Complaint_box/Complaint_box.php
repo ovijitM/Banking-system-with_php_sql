6caa7c9b15414f2bd1df0705ee2b3d9a9c13f817
amio008 <amiomalakar744@gmail.com>
2024-09-22 23:54:24 +0600

    Add files via upload
    Complaint_box: Front end ; Complaint: Back end

---
+++ User/Complaint_box/Complaint_box.php
@@ -0,0 +1,38 @@
+
+<?php 
+    
+    $username = $_POST['username'];
+    $account_number = $_POST['account_number'];
+    
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complaint Box</title>
+    
+</head>
+<body>
+    <form action="Complaint.php" method="post">
+        
+        <h2>Submit Complaint</h2>
+        <label for="cause">Complaint:</label>
+        <textarea id="cause" name="cause" required></textarea><br>
+        <input type='hidden' name='account_number' value='<?php echo"$account_number";?>'>
+        <input type='hidden' name='username' value='<?php echo"$username"?>'>
+        <button type="submit" name="submit_complaint">Submit Complaint</button>
+    </form>
+</body>
+</html>
+
+
+
+
