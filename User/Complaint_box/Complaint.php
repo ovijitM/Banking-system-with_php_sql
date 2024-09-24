93d727acb17bb00e12f2faadea2a3fee43910991
Debashish Sarker <136188559+Debashish0Sarker@users.noreply.github.com>
2024-09-24 13:16:32 +0600

    Update Complaint.php

--- User/Complaint_box/Complaint.php
+++ User/Complaint_box/Complaint.php
@@ -1,24 +1,16 @@
 <?php
-// Start the session
 //session_start();
 
-// Database connection
 @require "../connectserver.php";
 
-// Ensure the user is logged in and their details are available
 //if (!isset($_SESSION['username']) || !isset($_SESSION['account_number'])) {
 //die("Please log in to submit a complaint.");
 //
 
-// Fetch username and account number from the session
 $username = $_POST['username'];
 $account_number = $_POST['account_number'];
 $cause = $_POST['cause'];
 
-
-
-
-// Insert complaint into the database
 $complaint_query = "INSERT INTO complain_box (username, account_number, cause, status) 
                         VALUES ('$username', '$account_number', '$cause', 0)";
 
